bd727e9c706ff7407478b4ab757db1f378f30e77
Adam Pickering <adam@rocketgenius.com>
2026-03-25 11:36:01 -0400

    Scaffold Vite + React + Tailwind + UUI project with AM brand tokens

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-am-light font-sans">
+      <h1 className="text-am-navy text-2xl font-bold p-8">Release Radar</h1>
+    </div>
+  )
+}
+
+export default App
